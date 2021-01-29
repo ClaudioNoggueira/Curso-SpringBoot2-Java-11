a3b4043850de658508e6d4e5dee5b7e5ffe3ffc2
Claudio Nogueira <cllaudionoggueira@gmail.com>
2021-01-29 17:11:32 -0300

    Camada de serviço, registro de componentes

---
+++ src/main/java/com/claudio/course/services/UserService.java
@@ -0,0 +1,27 @@
+package com.claudio.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.claudio.course.entities.User;
+import com.claudio.course.repositories.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll(){
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+	
+}
