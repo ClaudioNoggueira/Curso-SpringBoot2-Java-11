4dd976af2b641d90f1dfe61e5ea3154bed6b41bd
Claudio Nogueira <cllaudionoggueira@gmail.com>
2021-01-29 15:54:14 -0300

    Entidade User e seu resource

---
+++ src/main/java/com/claudio/course/resources/UserResource.java
@@ -0,0 +1,18 @@
+package com.claudio.course.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.claudio.course.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+	@GetMapping
+	public ResponseEntity<User> findAll() {
+		User u = new User(1L, "Maria", "maria@email.com", "9889823421", "12345");
+		return ResponseEntity.ok().body(u);
+	}
+}
